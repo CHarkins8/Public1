dd680bb0fe93acc9639b0be10a30ac7f59be2da9
Cassandra Harkins <49220476+CHarkins8@users.noreply.github.com>
2019-07-19 08:28:56 -0400

    Create Arrays.pl

---
+++ perl/Arrays.pl
@@ -0,0 +1,43 @@
+
+#!/home/charkins/HelloWorld
+
+use strict;
+use warnings;
+
+my @array = (
+    "print ",
+    "these ",
+    "strings ",
+    "out ",
+    "for ",
+    "me ",
+);
+
+my $string = "Hello World!";
+
+print $array[0];
+print $array[1];
+print $array[2];
+print $array[3];
+print $array[4];
+print $array[5];
+# returns undef, prints "" and raises a warning
+print $array[6];
+
+print "\n";
+
+print $array[-1];
+print $array[-2];
+print $array[-3];
+print $array[-4];
+print $array[-5];
+print $array[-6];
+# returns undef, prints "" and raises a warning
+print $array[-7];
+
+print "\n";
+
+print "String: $string How are you?\n";
+print "Array: @array \n";
+print "\@array \n";
+print '@array' . "\n";
